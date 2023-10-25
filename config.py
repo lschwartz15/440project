4ae9cf47be62fabce331ab54df25c5ce649aba02
lschwartz15 <145065386+lschwartz15@users.noreply.github.com>
2023-10-25 08:06:19 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+
+#!!! Be sure to change these directory information 
+CONTENT_DIR = '/Users/Lex/Riki/content'
+USER_DIR = '/Users/Lex/Riki/user'
+
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
---
+++ content/home/Homesteader.md
@@ -0,0 +1,4 @@
+title: Homesteader
+tags: 
+
+A person who lives off of the land. 
