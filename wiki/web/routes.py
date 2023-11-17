840e185ccda83ce46fe621d44a4aad6fd0abf83d
Eric Jackman <jackmane1@mymail.nku.edu>
2023-11-17 04:19:09 -0500

    Added edit log to editor screen and implemented restore old versions of a page feature

--- wiki/web/routes.py
+++ wiki/web/routes.py
@@ -2,6 +2,8 @@
     Routes
     ~~~~~~
 """
+import os
+
 from flask import Blueprint
 from flask import flash
 from flask import redirect
@@ -14,6 +16,7 @@ from flask_login import login_user
 from flask_login import logout_user
 
 from wiki.core import Processor
+from wiki.core import PageVersionManager
 from wiki.web.forms import EditorForm
 from wiki.web.forms import LoginForm
 from wiki.web.forms import SearchForm
@@ -21,7 +24,7 @@ from wiki.web.forms import URLForm
 from wiki.web import current_wiki
 from wiki.web import current_users
 from wiki.web.user import protect
-
+from config import CONTENT_DIR
 
 bp = Blueprint('wiki', __name__)
 
@@ -29,145 +32,177 @@ bp = Blueprint('wiki', __name__)
 @bp.route('/')
 @protect
 def home():
-    page = current_wiki.get('home')
-    if page:
-        return display('home')
-    return render_template('home.html')
+	page = current_wiki.get('home')
+	if page:
+		return display('home')
+	return render_template('home.html')
 
 
 @bp.route('/index/')
 @protect
 def index():
-    pages = current_wiki.index()
-    return render_template('index.html', pages=pages)
+	pages = current_wiki.index()
+	return render_template('index.html', pages=pages)
 
 
 @bp.route('/<path:url>/')
 @protect
 def display(url):
-    page = current_wiki.get_or_404(url)
-    return render_template('page.html', page=page)
+	page = current_wiki.get_or_404(url)
+	return render_template('page.html', page=page)
 
 
 @bp.route('/create/', methods=['GET', 'POST'])
 @protect
 def create():
-    form = URLForm()
-    if form.validate_on_submit():
-        return redirect(url_for(
-            'wiki.edit', url=form.clean_url(form.url.data)))
-    return render_template('create.html', form=form)
+	form = URLForm()
+	if form.validate_on_submit():
+		return redirect(url_for(
+			'wiki.edit', url=form.clean_url(form.url.data)))
+	return render_template('create.html', form=form)
 
 
 @bp.route('/edit/<path:url>/', methods=['GET', 'POST'])
 @protect
 def edit(url):
-    page = current_wiki.get(url)
-    form = EditorForm(obj=page)
-    if form.validate_on_submit():
-        if not page:
-            page = current_wiki.get_bare(url)
-        form.populate_obj(page)
-        page.save()
-        flash('"%s" was saved.' % page.title, 'success')
-        return redirect(url_for('wiki.display', url=url))
-    return render_template('editor.html', form=form, page=page)
+	page = current_wiki.get(url)
+	pageVersionManager = PageVersionManager(url, current_user.name)
+	form = EditorForm(obj=page)
+	if form.validate_on_submit():
+		if not page:
+			page = current_wiki.get_bare(url)
+		if os.path.exists(CONTENT_DIR + '/' + url + '.md'):
+			form.populate_obj(page)
+			page.save()
+			pageVersionManager.update_page()
+		else:
+			form.populate_obj(page)
+			page.save()
+			pageVersionManager.create_page()
+		flash('"%s" was saved.' % page.title, 'success')
+		return redirect(url_for('wiki.display', url=url))
+	return render_template('editor.html', form=form, page=page)
+
+
+@bp.route('/edit/<path:url>/<path:version>/', methods=['GET', 'POST'])
+@protect
+def restore(url, version):
+	version = version.replace('_', ' ', 3).replace('_', ':')
+	pageVersionManager = PageVersionManager(url, current_user.name)
+	pageVersionManager.restore_page(version)
+	flash('Version from "%s" was restored.' % version, 'success')
+	return redirect(url_for('wiki.display', url=url))
 
 
 @bp.route('/preview/', methods=['POST'])
 @protect
 def preview():
-    data = {}
-    processor = Processor(request.form['body'])
-    data['html'], data['body'], data['meta'] = processor.process()
-    return data['html']
+	data = {}
+	processor = Processor(request.form['body'])
+	data['html'], data['body'], data['meta'] = processor.process()
+	return data['html']
+
+
+@bp.route('/history/<path:url>/', methods=['POST'])
+@protect
+def history(url):
+	pageVersionManager = PageVersionManager(url, current_user.name)
+	edits = pageVersionManager.get_edits()
+	edit_list = '<ul>'
+	for edit in edits:
+		version = edit.replace(' ', '_').replace(':', '_')
+		edit_list += '<li><a href="' + version + '" id="restorelink">' + edit + ' - ' + edits[edit]['user'] + '</a></li>'
+	edit_list += '</ul>'
+	return edit_list
 
 
 @bp.route('/move/<path:url>/', methods=['GET', 'POST'])
 @protect
 def move(url):
-    page = current_wiki.get_or_404(url)
-    form = URLForm(obj=page)
-    if form.validate_on_submit():
-        newurl = form.url.data
-        current_wiki.move(url, newurl)
-        return redirect(url_for('wiki.display', url=newurl))
-    return render_template('move.html', form=form, page=page)
+	page = current_wiki.get_or_404(url)
+	form = URLForm(obj=page)
+	if form.validate_on_submit():
+		newurl = form.url.data
+		current_wiki.move(url, newurl)
+		return redirect(url_for('wiki.display', url=newurl))
+	return render_template('move.html', form=form, page=page)
 
 
 @bp.route('/delete/<path:url>/')
 @protect
 def delete(url):
-    page = current_wiki.get_or_404(url)
-    current_wiki.delete(url)
-    flash('Page "%s" was deleted.' % page.title, 'success')
-    return redirect(url_for('wiki.home'))
+	page = current_wiki.get_or_404(url)
+	pageVersionManager = PageVersionManager(url, current_user.name)
+	pageVersionManager.delete_page()
+	current_wiki.delete(url)
+	flash('Page "%s" was deleted.' % page.title, 'success')
+	return redirect(url_for('wiki.home'))
 
 
 @bp.route('/tags/')
 @protect
 def tags():
-    tags = current_wiki.get_tags()
-    return render_template('tags.html', tags=tags)
+	tags = current_wiki.get_tags()
+	return render_template('tags.html', tags=tags)
 
 
 @bp.route('/tag/<string:name>/')
 @protect
 def tag(name):
-    tagged = current_wiki.index_by_tag(name)
-    return render_template('tag.html', pages=tagged, tag=name)
+	tagged = current_wiki.index_by_tag(name)
+	return render_template('tag.html', pages=tagged, tag=name)
 
 
 @bp.route('/search/', methods=['GET', 'POST'])
 @protect
 def search():
-    form = SearchForm()
-    if form.validate_on_submit():
-        results = current_wiki.search(form.term.data, form.ignore_case.data)
-        return render_template('search.html', form=form,
-                               results=results, search=form.term.data)
-    return render_template('search.html', form=form, search=None)
+	form = SearchForm()
+	if form.validate_on_submit():
+		results = current_wiki.search(form.term.data, form.ignore_case.data)
+		return render_template('search.html', form=form,
+							   results=results, search=form.term.data)
+	return render_template('search.html', form=form, search=None)
 
 
 @bp.route('/user/login/', methods=['GET', 'POST'])
 def user_login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = current_users.get_user(form.name.data)
-        login_user(user)
-        user.set('authenticated', True)
-        flash('Login successful.', 'success')
-        return redirect(request.args.get("next") or url_for('wiki.index'))
-    return render_template('login.html', form=form)
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = current_users.get_user(form.name.data)
+		login_user(user)
+		user.set('authenticated', True)
+		flash('Login successful.', 'success')
+		return redirect(request.args.get("next") or url_for('wiki.index'))
+	return render_template('login.html', form=form)
 
 
 @bp.route('/user/logout/')
 @login_required
 def user_logout():
-    current_user.set('authenticated', False)
-    logout_user()
-    flash('Logout successful.', 'success')
-    return redirect(url_for('wiki.index'))
+	current_user.set('authenticated', False)
+	logout_user()
+	flash('Logout successful.', 'success')
+	return redirect(url_for('wiki.index'))
 
 
 @bp.route('/user/')
 def user_index():
-    pass
+	pass
 
 
 @bp.route('/user/create/')
 def user_create():
-    pass
+	pass
 
 
 @bp.route('/user/<int:user_id>/')
 def user_admin(user_id):
-    pass
+	pass
 
 
 @bp.route('/user/delete/<int:user_id>/')
 def user_delete(user_id):
-    pass
+	pass
 
 
 """
@@ -178,5 +213,4 @@ def user_delete(user_id):
 
 @bp.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
-
+	return render_template('404.html'), 404
