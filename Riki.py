cc5f716db30a3c4248824e82a6e63b664fe7454b
Lex <lschwartz153@gmail.com>
2023-12-03 14:11:19 -0500

    Added design doc and new libraries to requirements.txt

--- Riki.py
+++ Riki.py
@@ -2,8 +2,8 @@ import os
 from wiki import create_app
 
 directory = os.getcwd()
-app = create_app(directory)
 testing_app = create_app(directory, testing=True)
+app = create_app(directory)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5001)
